b0b4090e5f1cf5c9656898d63094b7e44f3807c2
Natalie <npvincent1987@gmail.com>
2026-01-29 15:10:49 -0700

    Initial commit

---
+++ src/main/java/net/seraph/castleofglass/CastleOfGlassDataGenerator.java
@@ -0,0 +1,11 @@
+package net.seraph.castleofglass;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CastleOfGlassDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/seraph/castleofglass/CastleofGlassClient.java
@@ -0,0 +1,10 @@
+package net.seraph.castleofglass;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CastleofGlassClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
